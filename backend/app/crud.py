daffe103a41c932c7ca2428847f3962dfa70bbd1
Max Platonov <bliamba2000@gmail.com>
2024-11-15 13:24:58 +0400

    в crud.py добавил проверку на есть ли клиент с email

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,5 +1,5 @@
-from sqlmodel import Session
-
+from sqlmodel import Session, select
+from typing import Optional
 from .core.security import get_password_hash
 from .models import ClientCreate, Client
 
@@ -12,3 +12,9 @@ async def create_client(*, session: Session, client_create: ClientCreate) -> Cli
     session.commit()
     session.refresh(db_obj)
     return db_obj
+
+
+async def get_client_by_email(*, session: Session, email: str) -> Optional[Client]:
+    statement = select(Client).where(Client.email == email)
+    session_client = session.exec(statement).first()
+    return session_client
