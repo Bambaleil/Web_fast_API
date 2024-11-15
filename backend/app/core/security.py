d5215e3ce3625e55327c107065f6efa54f909c7a
Max Platonov <bliamba2000@gmail.com>
2024-11-15 15:54:40 +0400

    добавил ALGORITHM

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,7 +1,8 @@
-
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+ALGORITHM = "HS256"
 
 
 async def get_password_hash(password: str) -> str:
