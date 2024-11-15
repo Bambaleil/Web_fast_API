54d1c9db32ea25e6d746005bb028943a9f99e3d0
Max Platonov <bliamba2000@gmail.com>
2024-11-15 15:49:14 +0400

    Добавил get_like_between_clients для проверки существования записи

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,7 +1,9 @@
+import uuid
+
 from sqlmodel import Session, select
 from typing import Optional
 from .core.security import get_password_hash
-from .models import ClientCreate, Client
+from .models import ClientCreate, Client, Like
 
 
 async def create_client(*, session: Session, client_create: ClientCreate) -> Client:
@@ -15,6 +17,12 @@ async def create_client(*, session: Session, client_create: ClientCreate) -> Cli
 
 
 async def get_client_by_email(*, session: Session, email: str) -> Optional[Client]:
-    statement = select(Client).where(Client.email == email)
-    session_client = session.exec(statement).first()
+    statement: select = select(Client).where(Client.email == email)
+    session_client: Optional[Client] = session.exec(statement).first()
     return session_client
+
+
+async def get_like_between_clients(*, session: Session, liker_id: uuid.UUID, liked_id: uuid.UUID) -> Optional[Like]:
+    statement: select = select(Like).where(Like.liker_id == liker_id and Like.liked_id == liked_id)
+    session_like: Optional[Like] = session.exec(statement).first()
+    return session_like
