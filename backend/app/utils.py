6fa35ec84f9f24b29efd79cc3eae266e30d9444d
Max Platonov <bliamba2000@gmail.com>
2024-11-15 13:25:52 +0400

    Добавил logging

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,22 +1,86 @@
+import logging
+from dataclasses import dataclass
 from enum import Enum
 from io import BytesIO
 from pathlib import Path
-from typing import Optional
-
+from typing import Optional, Any
+import emails
+from jinja2 import Template
 import aiofiles
 from PIL import Image, ImageEnhance
 from fastapi.security import OAuth2PasswordBearer
-from passlib.context import CryptContext
+
+from .core.config import settings
 
 project_root = Path(__file__).resolve().parents[2]
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class SexEnum(str, Enum):
     male = "мужской"
     female = "женский"
 
+
+# Все что связанно с редером и отправкой имейла новому пользователю
+# @dataclass
+# class EmailData:
+#     html_content: str
+#     subject: str
+#
+#
+# def render_email_template(*, template_name: str, context: dict[str, Any]) -> str:
+#     template_str = (
+#             Path(__file__).parent / "email-templates" / "build" / template_name
+#     ).read_text()
+#     html_content = Template(template_str).render(context)
+#     return html_content
+#
+#
+# def send_email(
+#         *,
+#         email_to: str,
+#         subject: str = "",
+#         html_content: str = "",
+# ) -> None:
+#     assert settings.emails_enabled, "no provided configuration for email variables"
+#     message = emails.Message(
+#         subject=subject,
+#         html=html_content,
+#         mail_from=(settings.EMAILS_FROM_NAME, settings.EMAILS_FROM_EMAIL),
+#     )
+#     smtp_options = {"host": settings.SMTP_HOST, "port": settings.SMTP_PORT}
+#     if settings.SMTP_TLS:
+#         smtp_options["tls"] = True
+#     elif settings.SMTP_SSL:
+#         smtp_options["ssl"] = True
+#     if settings.SMTP_USER:
+#         smtp_options["user"] = settings.SMTP_USER
+#     if settings.SMTP_PASSWORD:
+#         smtp_options["password"] = settings.SMTP_PASSWORD
+#     response = message.send(to=email_to, smtp=smtp_options)
+#     logger.info(f"send email result: {response}")
+#
+#
+# def generate_new_account_email(
+#         email_to: str, client_name: str, password: str
+# ) -> EmailData:
+#     project_name = settings.PROJECT_NAME
+#     subject = f"{project_name} - New account for client {client_name}"
+#     html_content = render_email_template(
+#         template_name="new_account.html",
+#         context={
+#             "project_name": settings.PROJECT_NAME,
+#             "username": client_name,
+#             "password": password,
+#             "email": email_to,
+#             "link": settings.FRONTEND_HOST,
+#         },
+#     )
+#     return EmailData(html_content=html_content, subject=subject)
 
 def get_project_root() -> Path:
     return Path(__file__).resolve().parents[2]
@@ -32,10 +96,6 @@ async def add_watermark_with_photo(avatar: Optional[bytes] = None,
                                    opacity: float = 0.1) -> bytes:
     if watermark_path is None:
         watermark_path = get_image("watermark.jpg")
-    # if avatar is None:
-    #     avatar_path = get_image("default_avatar.jpeg")
-    #     async with aiofiles.open(avatar_path, "rb") as f:
-    #         avatar = await f.read()
 
     async with aiofiles.open(watermark_path, "rb") as f:
         watermark_bytes = await f.read()
