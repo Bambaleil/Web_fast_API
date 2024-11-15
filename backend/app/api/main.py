c2878f424bdf29e28489a17bad9054c14d51f99a
Max Platonov <bliamba2000@gmail.com>
2024-11-15 17:34:05 +0400

    добавил routes like

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from .routes import clients
+from .routes import clients, likes
 
 api_router = APIRouter()
 
 api_router.include_router(clients.router, prefix="/clients", tags=["clients"])
+api_router.include_router(likes.router, prefix="/likes", tags=["likes"])
