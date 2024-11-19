eee284028c97168bdc1781d67707bc70f7866228
Max Platonov <bliamba2000@gmail.com>
2024-11-19 12:12:53 +0400

    дописал логику рендеринга и отправки email

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -3,12 +3,13 @@ from dataclasses import dataclass
 from enum import Enum
 from io import BytesIO
 from pathlib import Path
-from typing import Optional, Any
-import emails
-from jinja2 import Template
+from typing import Optional, Any, Tuple
+
 import aiofiles
+import emails
 from PIL import Image, ImageEnhance
 from fastapi.security import OAuth2PasswordBearer
+from jinja2 import Template
 
 from .core.config import settings
 
@@ -24,63 +25,75 @@ class SexEnum(str, Enum):
     male = "мужской"
     female = "женский"
 
+@dataclass
+class EmailData:
+    html_content: str
+    subject: str
 
-# Все что связанно с редером и отправкой имейла новому пользователю
-# @dataclass
-# class EmailData:
-#     html_content: str
-#     subject: str
-#
-#
-# def render_email_template(*, template_name: str, context: dict[str, Any]) -> str:
-#     template_str = (
-#             Path(__file__).parent / "email-templates" / "build" / template_name
-#     ).read_text()
-#     html_content = Template(template_str).render(context)
-#     return html_content
-#
-#
-# def send_email(
-#         *,
-#         email_to: str,
-#         subject: str = "",
-#         html_content: str = "",
-# ) -> None:
-#     assert settings.emails_enabled, "no provided configuration for email variables"
-#     message = emails.Message(
-#         subject=subject,
-#         html=html_content,
-#         mail_from=(settings.EMAILS_FROM_NAME, settings.EMAILS_FROM_EMAIL),
-#     )
-#     smtp_options = {"host": settings.SMTP_HOST, "port": settings.SMTP_PORT}
-#     if settings.SMTP_TLS:
-#         smtp_options["tls"] = True
-#     elif settings.SMTP_SSL:
-#         smtp_options["ssl"] = True
-#     if settings.SMTP_USER:
-#         smtp_options["user"] = settings.SMTP_USER
-#     if settings.SMTP_PASSWORD:
-#         smtp_options["password"] = settings.SMTP_PASSWORD
-#     response = message.send(to=email_to, smtp=smtp_options)
-#     logger.info(f"send email result: {response}")
-#
-#
-# def generate_new_account_email(
-#         email_to: str, client_name: str, password: str
-# ) -> EmailData:
-#     project_name = settings.PROJECT_NAME
-#     subject = f"{project_name} - New account for client {client_name}"
-#     html_content = render_email_template(
-#         template_name="new_account.html",
-#         context={
-#             "project_name": settings.PROJECT_NAME,
-#             "username": client_name,
-#             "password": password,
-#             "email": email_to,
-#             "link": settings.FRONTEND_HOST,
-#         },
-#     )
-#     return EmailData(html_content=html_content, subject=subject)
+
+async def render_email_template(*, template_name: str, context: dict[str, Any]) -> str:
+    template_str = (
+            Path(__file__).parent / "email-templates" / "build" / template_name
+    ).read_text()
+    html_content = Template(template_str).render(context)
+    return html_content
+
+
+async def send_email(
+        *,
+        email_to: str,
+        subject: str = "",
+        html_content: str = "",
+) -> None:
+    assert settings.emails_enabled, "no provided configuration for email variables"
+    message = emails.Message(
+        subject=subject,
+        html=html_content,
+        mail_from=(settings.EMAILS_FROM_NAME, settings.EMAILS_FROM_EMAIL),
+    )
+    smtp_options = {"host": settings.SMTP_HOST, "port": settings.SMTP_PORT}
+    if settings.SMTP_TLS:
+        smtp_options["tls"] = False
+    if settings.SMTP_SSL:
+        smtp_options["ssl"] = True
+    if settings.SMTP_USER:
+        smtp_options["user"] = settings.SMTP_USER
+    if settings.SMTP_PASSWORD:
+        smtp_options["password"] = settings.SMTP_PASSWORD
+    response = message.send(to=email_to, smtp=smtp_options)
+    logger.info(f"send email result: {response}")
+    if response.status_code is None or response.status_code != 250:
+        logger.error(
+            f"Failed to send email. Status code: {response.status_code}, Status text: {response.status_text}")
+    else:
+        logger.info(f"Email sent successfully! Status code: {response.status_code}")
+
+
+async def generate_match_email(liker_obj, liked_obj) -> Tuple[EmailData, EmailData]:
+    project_name = settings.PROJECT_NAME
+    subject = f"{project_name} - New match between clients."
+    html_content_1 = await render_email_template(
+        template_name="new_between_match.html",
+        context={
+            "project_name": settings.PROJECT_NAME,
+            "username": liker_obj.name,
+            "username_2": liked_obj.name,
+            "email_2": liked_obj.email,
+        }
+    )
+
+    html_content_2 = await render_email_template(
+        template_name="new_between_match.html",
+        context={
+            "project_name": settings.PROJECT_NAME,
+            "username": liked_obj.name,
+            "username_2": liker_obj.name,
+            "email_2": liker_obj.email,
+        }
+    )
+    return (EmailData(html_content=html_content_1, subject=subject),
+            EmailData(html_content=html_content_2, subject=subject))
+
 
 def get_project_root() -> Path:
     return Path(__file__).resolve().parents[2]
