b4df3197eda3179226f142a6f70bf8b20b0f8b6a
Max Platonov <bliamba2000@gmail.com>
2024-11-21 16:26:54 +0400

    добавил фильтр по дистанции и кеш, авторизацию и обновление клиента

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,13 +1,27 @@
 import uuid
-from typing import Optional, Sequence
+from typing import Optional, Sequence, List
 
+from aiocache import cached
 from sqlmodel import Session, select
 
-from .core.security import get_password_hash
-from .models import ClientCreate, Client, Like, FilterClient
+from .core.security import get_password_hash, verify_password
+from .models import ClientCreate, Client, Like, FilterClient, ClientUpdateLocation
+from .utils import great_circle_distance
 
 
-async def get_list_filter_client(*, session: Session, filter_client: FilterClient):
+@cached(ttl=600)
+async def get_clients_within_distance(*, session: Session, current_lat: float, current_lon: float, distance: float,
+                                      current_email: str) -> List[Client]:
+    statement = select(Client).where(Client.latitude.isnot(None), Client.longitude.isnot(None),
+                                     Client.email != current_email)
+    clients_within_distance: List[Client.id] = [
+        client.id for client in session.exec(statement).all() if distance >= great_circle_distance(
+            lat1=current_lat, lon1=current_lon, lat2=client.latitude, lon2=client.longitude
+        )]
+    return clients_within_distance
+
+
+async def get_list_filter_client(*, session: Session, filter_client: FilterClient, current_client):
     statement = select(Client)
     if filter_client.sex:
         statement = statement.where(Client.sex == filter_client.sex)
@@ -15,6 +29,13 @@ async def get_list_filter_client(*, session: Session, filter_client: FilterClien
         statement = statement.where(Client.name.ilike(f"%{filter_client.name}%"))
     if filter_client.surname:
         statement = statement.where(Client.surname.ilike(f"%{filter_client.surname}%"))
+    if filter_client.distance:
+        clients_within_distance_ids = await get_clients_within_distance(session=session,
+                                                                        current_lat=current_client.latitude,
+                                                                        current_lon=current_client.longitude,
+                                                                        distance=filter_client.distance,
+                                                                        current_email=current_client.email)
+        statement = statement.where(Client.id.in_(clients_within_distance_ids))
     clients: Sequence[Optional[Client]] = session.exec(statement).all()
     return clients
 
@@ -27,6 +48,20 @@ async def create_client(*, session: Session, client_create: ClientCreate) -> Cli
     session.commit()
     session.refresh(db_obj)
     return db_obj
+
+
+async def update_client(*, session: Session, db_client: Client, client_up: ClientUpdateLocation) -> Client:
+    client_data = client_up.model_dump(exclude_unset=True)
+    extra_data = {}
+    if "password" in client_data:
+        password = client_data["password"]
+        hashed_password = get_password_hash(password)
+        extra_data["hashed_password"] = hashed_password
+    db_client.sqlmodel_update(client_data, update=extra_data)
+    session.add(db_client)
+    session.commit()
+    session.refresh(db_client)
+    return db_client
 
 
 async def get_client_by_email(*, session: Session, email: str) -> Optional[Client]:
@@ -70,3 +105,12 @@ async def update_match_user(*, session: Session, liked_obj: Like, liker_obj: Lik
     liked_obj.match = True
     session.bulk_save_objects([liker_obj, liked_obj])
     session.commit()
+
+
+async def authenticate(*, session: Session, email: str, password: str) -> Optional[Client]:
+    db_client: Optional[Client] = await get_client_by_email(session=session, email=email)
+    if not db_client:
+        return None
+    if not verify_password(password, db_client.hashed_password):
+        return None
+    return db_client
