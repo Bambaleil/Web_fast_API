b23ff1957373bc7b32389d375b302be731765856
Max Platonov <bliamba2000@gmail.com>
2024-11-21 16:27:16 +0400

    переименовал

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,20 +1,21 @@
 from sqlmodel import Session, create_engine, select
 
-from .. import crud
 from .config import settings
+
+from .. import crud
 from ..models import Client, ClientCreate
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
 
 def init_db(session: Session) -> None:
-    user = session.exec(
+    client = session.exec(
         select(Client).where(Client.email == settings.FIRST_SUPERUSER)
     ).first()
-    if not user:
+    if not client:
         client_in = ClientCreate(
             email=settings.FIRST_SUPERUSER,
             password=settings.FIRST_SUPERUSER_PASSWORD,
             is_superuser=True,
         )
-        user = crud.create_client(session=session, client_create=client_in)
+        client = crud.create_client(session=session, client_create=client_in)
