e65edf74fc184babb6c24327c220b6e5823f640a
Max Platonov <bliamba2000@gmail.com>
2024-11-21 16:29:23 +0400

    добавлен логин

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from .routes import clients, likes
+from .routes import clients, likes, login
 
 api_router = APIRouter()
 
+api_router.include_router(login.router, tags=["login"])
 api_router.include_router(clients.router, prefix="/clients", tags=["clients"])
 api_router.include_router(likes.router, prefix="/likes", tags=["likes"])
