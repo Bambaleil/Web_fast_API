ef302c0220a0a6f69ec32137656c24170454f906
Max Platonov <bliamba2000@gmail.com>
2024-11-21 16:31:19 +0400

    проверка пароля и создание токена

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,4 +1,10 @@
+from datetime import timedelta, datetime, timezone
+from typing import Any
+
+import jwt
 from passlib.context import CryptContext
+
+from app.core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +13,14 @@ ALGORITHM = "HS256"
 
 async def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode = {"exp": expire, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
