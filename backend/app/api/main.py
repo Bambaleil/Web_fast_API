3722a410102bbf3696368d621bdfd6fd27e98b24
Max Platonov <bliamba2000@gmail.com>
2024-12-13 17:15:43 +0400

    добавил utils

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from .routes import clients, likes, login
+from .routes import clients, likes, login, utils
 
 api_router = APIRouter()
 
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(clients.router, prefix="/clients", tags=["clients"])
+api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(likes.router, prefix="/likes", tags=["likes"])
